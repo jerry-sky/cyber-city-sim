51535c9bba1c64f66a63c351c606e326d2fbdeb6
jerry-sky <jwroczynski@gmail.com>
2020-11-14 21:59:30 +0100

    model: added error server response

--- model/server-responses.ts
+++ model/server-responses.ts
@@ -1,5 +1,10 @@
+import { Errors } from './errors';
 import { User } from './user';
 
 export interface LoginResponse {
     user: User;
+}
+
+export interface ErrorResponse {
+    errorCode: Errors;
 }
