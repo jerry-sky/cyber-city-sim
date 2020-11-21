e40a3bb5fa108dddc0656da8136ec6f7c976ba06
kamilmatejuk <kamilmatejuk@gmail.com>
2020-11-21 20:23:23 +0100

    [client] Added ingame time counting

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public inGameTime: Date;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.inGameTime = new Date('00:00 01/01/2170');
+    window.setInterval(this.changeTime.bind(this), 1000);
+  }
+
+  changeTime(): void {
+    this.inGameTime = new Date(this.inGameTime.getTime() + 60 * 1000);  // 1 minute, each second
   }
 
 }
