1f294a4b14ae0785d4fe9e99fe3dda33eda70e04
kamilmatejuk <kamilmatejuk@gmail.com>
2020-11-22 04:28:15 +0100

    [client] Added building info popup

---
+++ client/src/app/building-info-popup/building-info-popup.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BuildingInfoPopupComponent } from './building-info-popup.component';
+
+describe('BuildingInfoPopupComponent', () => {
+  let component: BuildingInfoPopupComponent;
+  let fixture: ComponentFixture<BuildingInfoPopupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BuildingInfoPopupComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BuildingInfoPopupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/building-info-popup/building-info-popup.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+
+export interface BuildingInfoDialogData {
+  buildingName: string;
+  before: {
+    red: number,
+    green: number,
+    blue: number
+  };
+  after: {
+    red: number,
+    green: number,
+    blue: number
+  };
+}
+
+@Component({
+  selector: 'app-building-info-popup',
+  templateUrl: './building-info-popup.component.html',
+  styleUrls: ['./building-info-popup.component.scss']
+})
+export class BuildingInfoPopupComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<BuildingInfoPopupComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: BuildingInfoDialogData
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  goBack(): void {
+    this.dialogRef.close(false);
+  }
+
+  upgrade(): void {
+    this.dialogRef.close(true);
+  }
+
+}
