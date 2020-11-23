c9a1ff6d3ae886d1e0a0cb0fbd6c25e9f7e8154f
jerry-sky <jwroczynski@gmail.com>
2020-11-23 19:40:36 +0100

    server: database: set the character to 4-byte unicode

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,6 +1,6 @@
 -- Create the default database access for a regular player.
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
-CREATE DATABASE IF NOT EXISTS ccs;
+CREATE DATABASE IF NOT EXISTS ccs CHARACTER SET = utf8mb4;
 
 -- Grant privileges to the user: SELECT for verifying their credentials,
 -- INSERT to allow registration and UPDATE to change the credentials.
