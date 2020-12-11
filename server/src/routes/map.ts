812c296dcc1b89989e74b1d4ce395cefb2c2094d
jerry-sky <jwroczynski@gmail.com>
2020-12-11 23:41:44 +0100

    server: refactored the map/ route

--- server/src/routes/map.ts
+++ server/src/routes/map.ts
@@ -1,52 +1,19 @@
 import { MapResponse } from '../../../model/server-responses';
 import { RouterWrapper } from '../auxiliary/express-method-wrapper';
-import { Map, Cell } from '../../../model/map';
+import { Map } from '../../../model/map';
+import { DatabaseService } from '../services/database.service';
 
 const Router = new RouterWrapper();
+const Database = new DatabaseService();
 
 Router.get<never, MapResponse, never>('/', async (request, response, next) => {
-  const cells: Cell[] = [];
-  for (let i = 0; i < 400; i++) {
-    const c: Cell = {
-      terrain: Math.floor(Math.random() * 3),
-      owner: -1,
-      buildingType: -1,
-      buildingLvl: 0,
-    };
-    cells.push(c);
-  }
-  // city 1
-  cells[30].owner = 1;
-  cells[31].owner = 1;
-  cells[32].owner = 1;
-  cells[50].owner = 1;
-  cells[51].owner = 1;
-  cells[52].owner = 1;
-  cells[30].buildingType = Math.floor(Math.random() * 3);
-  cells[31].buildingType = Math.floor(Math.random() * 3);
-  cells[32].buildingType = Math.floor(Math.random() * 3);
-  cells[52].buildingType = Math.floor(Math.random() * 3);
-  // city 2
-  cells[120].owner = 2;
-  cells[121].owner = 2;
-  cells[140].owner = 2;
-  cells[141].owner = 2;
-  cells[120].buildingType = Math.floor(Math.random() * 3);
-  cells[141].buildingType = Math.floor(Math.random() * 3);
-  // city 3
-  cells[325].owner = 3;
-  cells[326].owner = 3;
-  cells[345].owner = 3;
-  cells[346].owner = 3;
-  cells[365].owner = 3;
-  cells[366].owner = 3;
-  cells[366].buildingType = Math.floor(Math.random() * 3);
+  const map: Map = { cells: [] };
 
-  const map: Map = {
-    cells: cells,
-  } as Map;
+  Database.ExecuteInsideDatabaseHarness(async (connection) => {
+    map.cells = await connection.query('SELECT * FROM `map`');
+  });
+
   response.json({ map });
-
   next();
 });
 
