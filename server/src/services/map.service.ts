5e0f5b5cd742afcb5d9f38eb42f3dc64ce49469e
deatomizeer <przemczyk256@gmail.com>
2020-12-12 17:30:50 +0100

    server: added methods for assigning a cell to a user and checking if they control any

--- server/src/services/map.service.ts
+++ server/src/services/map.service.ts
@@ -1,14 +1,50 @@
-import { Map } from '../../../model/map';
+import { DatabaseTables } from '../../../model/database-tables';
+import { Cell, Map } from '../../../model/map';
+import { User } from '../../../model/user';
 import { DatabaseService } from './database.service';
 
 export class MapService {
   constructor(private DB: DatabaseService) {}
 
+  /**
+   * Load the world map data from the database.
+   */
   public async LoadWorldMap(): Promise<Map> {
     let map: Map;
     await this.DB.ExecuteInsideDatabaseHarness(async (connection) => {
       map.cells = await connection.query('SELECT * FROM `map`');
     });
     return map;
+  }
+  /**
+   * Check if the user controls any cells.
+   * Return true if they do, and false otherwise.
+   * @param user The user in question.
+   */
+  public async HasNoLand(user: User): Promise<boolean> {
+    let ownedLand: Cell[];
+    await this.DB.ExecuteInsideDatabaseHarness(async (connection) => {
+      ownedLand = await connection.query(
+        'SELECT * FROM ' + DatabaseTables.MAP + ' WHERE `id` = ?',
+        [user.id]
+      );
+    });
+    const n = ownedLand.length;
+    return n === 0;
+  }
+  /**
+   * Assign one cell of the map to a user.
+   * @param user The user that will be granted the cell.
+   * @param index The cell's index number.
+   * @param cell The cell object containing information about resources and the like.
+   */
+  //TODO: introduce cell's cost.
+  public async AssignCellToUser(user: User, index: number): Promise<void> {
+    await this.DB.ExecuteInsideDatabaseHarness(async (connection) => {
+      await connection.query(
+        'UPDATE ' + DatabaseTables.MAP + ' SET `owner` = ? WHERE `id` = ?',
+        [user.id, index]
+      );
+    });
   }
 }
