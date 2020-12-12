cf6395cd426aa876549108462c324ef697dc381c
jerry-sky <jwroczynski@gmail.com>
2020-12-12 15:16:59 +0100

    server: database: changed the database username

--- server/database/database.sql
+++ server/database/database.sql
@@ -1,10 +1,10 @@
 -- Create the default database access for a regular player.
-CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'ccs-user'@'localhost' IDENTIFIED BY 'password';
 CREATE DATABASE IF NOT EXISTS ccs CHARACTER SET = utf8mb4;
 
 -- Grant privileges to the user: SELECT for verifying their credentials,
 -- INSERT to allow registration and UPDATE to change the credentials.
-GRANT SELECT, INSERT, UPDATE ON ccs.* TO 'user'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON ccs.* TO 'ccs-user'@'localhost';
 FLUSH PRIVILEGES;
 
 USE ccs;
