760b21904798b39ee44c9453cd8e66f6bdb88ee3
jerry-sky <jwroczynski@gmail.com>
2020-12-14 22:58:40 +0100

    model: added the global chat id

--- model/chat.ts
+++ model/chat.ts
@@ -7,7 +7,9 @@ export interface Chat {
    */
   id: number;
   /**
-   * Whether the chat is private or global.
+   * Whether the chat is private or not (can be global or a group chat).
    */
   private: boolean;
 }
+
+export const GLOBAL_CHAT_ID = 1;
